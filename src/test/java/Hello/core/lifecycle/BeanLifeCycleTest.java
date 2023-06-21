6dcbbf858d10bc1b73fe0d094e67025c05ba719f
monsteralover <kmj99301@gmail.com>
2023-06-21 23:11:00 +0900

    @PostConstruct, @PreDestroy apply

--- src/test/java/Hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/Hello/core/lifecycle/BeanLifeCycleTest.java
@@ -1,7 +1,6 @@
 package hello.core.lifecycle;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +8,16 @@ import org.springframework.context.annotation.Configuration;
 public class BeanLifeCycleTest {
     @Test
     public void lifeCycleTest(){
-        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
         NetworkClient client = ac.getBean(NetworkClient.class);
         ac.close();
+
     }
+
     @Configuration
     static class LifeCycleConfig {
         @Bean
+        // (initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient(){
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
